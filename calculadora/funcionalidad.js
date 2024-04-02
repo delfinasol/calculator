c52d5940a2a9122252d7202de8eeea2b4a0a31eb
delfinanastaro@gmail.com <Moraylola1>
2024-04-02 03:00:38 -0300

    Initial commit

---
+++ calculadora/funcionalidad.js
@@ -0,0 +1,34 @@
+const pantalla = document.querySelector(".pantalla");
+const botones = document.querySelectorAll(".button");
+
+botones.forEach(boton => {
+    boton.addEventListener( "click", () => {
+        const botonApretado = boton.textContent;
+
+        if (boton.id === "c") {
+            pantalla.textContent = "0";
+            return;
+        }
+
+        if (boton.id === "borrar") {
+            if (pantalla.textContent.length === 1 || pantalla.textContent === "Error!") {
+                pantalla.textContent = "0";
+            } else {
+                pantalla.textContent = pantalla.textContent.slice(0, -1);
+            }
+            return;
+        }
+
+        if (boton.id === "igual") {
+            pantalla.textContent = eval(pantalla.textContent);
+            return;
+        }
+
+
+        if (pantalla.textContent === "0" || pantalla.textContent === "Error!") {
+            pantalla.textContent = botonApretado;
+        } else {
+            pantalla.textContent += botonApretado;
+        }
+    })
+})
